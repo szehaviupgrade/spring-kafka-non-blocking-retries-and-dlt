72bf1a9add86a279d707f7839959f715af7a65ce
szehaviupgrade <szehavi@upgrade.com>
2022-08-29 09:10:39 -0400

    print interval between events

--- src/main/java/com/example/kafka/nonblockingretries/RetryableKafkaListener.java
+++ src/main/java/com/example/kafka/nonblockingretries/RetryableKafkaListener.java
@@ -1,11 +1,13 @@
 package com.example.kafka.nonblockingretries;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
@@ -14,19 +16,24 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class RetryableKafkaListener {
 
-  @RetryableTopic(
-      attempts = "4",
-      backoff = @Backoff(delay = 1000, multiplier = 2.0),
-      autoCreateTopics = "false",
-      topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
-  @KafkaListener(topics = "orders")
-  public void listen(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-    log.info(in + " from " + topic);
-    throw new RuntimeException("test");
-  }
+    private long previous = 0;
 
-  @DltHandler
-  public void dlt(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-    log.info(in + " from " + topic);
-  }
+    @RetryableTopic(
+            attempts = "4",
+            backoff = @Backoff(delay = 1000, multiplier = 2.0),
+            autoCreateTopics = "false",
+            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
+    @KafkaListener(topics = "orders")
+    public void listen(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        var current = System.currentTimeMillis();
+        previous = previous == 0 ? current : previous;
+        var interval = current - previous;
+        log.info(in + " from " + topic + " topic interval = " + interval + " milliseconds since previous message");
+        throw new RuntimeException("test");
+    }
+
+    @DltHandler
+    public void dlt(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.info(in + " from DLT" + topic);
+    }
 }
